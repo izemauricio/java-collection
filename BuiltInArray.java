e10d5d6a0f92f660091629c1e2fffb5f939cf83e
Mauricio Ize <mauriciodcjz@gmail.com>
2020-03-06 15:51:17 -0300

    built in array

---
+++ BuiltInArray.java
@@ -0,0 +1,12 @@
+class BuiltInArray {
+    public static void main(String args[]) {
+        int[] arr = {1,2,3,4};
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        int[][] mat = {{1,2},{3,4},{5,6},{7,8}};
+        System.out.print(mat[3][0] + " ");
+    }
+}
